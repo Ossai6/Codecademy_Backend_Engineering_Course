8ef62e6eea5e881cdda327e939da70b7dca6adec
Ossai6 <calebossaiakpe@gmail.com>
2025-01-10 18:06:32 +0100

    Added project file

--- JavaScript Part 1/Number Guesser Project/script.js
+++ JavaScript Part 1/Number Guesser Project/script.js
@@ -4,3 +4,25 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => { return Math.floor(Math.random() * 10); };
+console.log(generateTarget());
+
+const compareGuesses = (userGuess, computerGuess, secretTarget) => {
+    if (userGuess === computerGuess) {
+        return true;
+    }
+
+    if (secretTarget > userGuess && secretTarget > computerGuess) {
+        if (userGuess > computerGuess) {
+            return true;
+        } else {
+            return false;
+        };
+    } else if (secretTarget < userGuess && secretTarget < computerGuess) {
+        if (userGuess < computerGuess) {
+            return true;
+        } else {
+            return false;
+        };
+    } else if (secretTarget < userGuess && secretTarget < computerGuess)
+};
